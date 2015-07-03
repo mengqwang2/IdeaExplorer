3decc0133e6bc6ccd4f2543394f5aa2fb2998205
bonnienyt <bonnieng1994@gmail.com>
2015-07-03 04:15:41 -0400

    ionic app
    side bar structure with UI enhancement

---
+++ www/js/controllers.js
@@ -0,0 +1,43 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+  };
+})
+
+.controller('PlaylistsCtrl', ['$scope', '$state', function($scope, $state) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+  $scope.whichplaylist = $state.params.playlistId;
+}])
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('SignInCtrl', function($scope, $state) {
+  
+  $scope.signIn = function(user) {
+    <!--console.log('Sign-In', user);-->
+    $state.go('app.home');
+  };
+  
+});
