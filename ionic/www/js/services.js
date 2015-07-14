7e03fc8b738fae79ec435c1b7cf0aabaa1ad5cfc
kch0217 <kch0217@yahoo.com.hk>
2015-07-14 17:07:13 +0800

    Add User Authentication

--- ionic/www/js/services.js
+++ ionic/www/js/services.js
@@ -4,4 +4,111 @@ angular.module('starter.services', []).factory('Idea', function($resource) {
       method: 'PUT'
     }
   });
-});
+})
+
+.factory('authentication', function($resource){
+	return $resource('http://localhost:port/api/login', { port: ':5000' });
+})
+
+.service('AuthService', function($q, $http, USER_ROLES, authentication) {
+  var LOCAL_TOKEN_KEY = 'yourTokenKey';
+  var username = '';
+  var isAuthenticated = false;
+  var role = '';
+  var authToken;
+ 
+  function loadUserCredentials() {
+    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+    if (token) {
+      useCredentials(token);
+    }
+  }
+ 
+  function storeUserCredentials(token) {
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
+    useCredentials(token);
+  }
+ 
+  function useCredentials(token) {
+    username = token.split('.')[0];
+    isAuthenticated = true;
+    authToken = token;
+ 
+    if (username == 'admin') {
+      role = USER_ROLES.admin
+    }
+    if (username == 'user') {
+      role = USER_ROLES.public
+    }
+ 
+    // Set the token as header for your requests!
+    $http.defaults.headers.common['X-Auth-Token'] = token;
+  }
+ 
+  function destroyUserCredentials() {
+  	console.log("destroying");
+    authToken = undefined;
+    username = '';
+    isAuthenticated = false;
+    $http.defaults.headers.common['X-Auth-Token'] = undefined;
+    window.localStorage.removeItem(LOCAL_TOKEN_KEY);
+  }
+ 
+  var login = function(name, pw) {
+  	console.log("login");
+    return $q(function(resolve, reject) {
+      if (name !== '' && pw !== '' ) {
+        // Make a request and receive your auth token from your server
+        var data = { "Username": name, "Password": pw};
+        authentication.save(data, function(content1){
+        	console.log(content1);
+        	storeUserCredentials(name + '.' + content1['Token']);
+        	resolve('Login success.');
+
+        });
+
+      } else {
+        reject('Login Failed.');
+      }
+    });
+  };
+ 
+  var logout = function() {
+    destroyUserCredentials();
+  };
+ 
+  var isAuthorized = function(authorizedRoles) {
+    if (!angular.isArray(authorizedRoles)) {
+      authorizedRoles = [authorizedRoles];
+    }
+    return (isAuthenticated && authorizedRoles.indexOf(role) !== -1);
+  };
+ 
+  loadUserCredentials();
+ 
+  return {
+    login: login,
+    logout: logout,
+    isAuthorized: isAuthorized,
+    isAuthenticated: function() {return isAuthenticated;},
+    username: function() {return username;},
+    role: function() {return role;}
+  };
+})
+
+
+.factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+  return {
+    responseError: function (response) {
+      $rootScope.$broadcast({
+        401: AUTH_EVENTS.notAuthenticated,
+        403: AUTH_EVENTS.notAuthorized
+      }[response.status], response);
+      return $q.reject(response);
+    }
+  };
+})
+ 
+.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('AuthInterceptor');
+});;
