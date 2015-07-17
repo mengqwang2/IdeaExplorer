2c5ed56de5cd07c3045a9e912f58048112af3c9e
WildVegeJuice <jiahushen2@gmail.com>
2015-07-17 18:48:29 +0900

    Update Recommendations.py

--- code/LDA/Recommendations.py
+++ code/LDA/Recommendations.py
@@ -1,6 +1,4 @@
 import numpy
-import sys, os, re, random, math, urllib2, time, cPickle
-import numpy
 import operator
 import dataParse
 import onlineldavb
@@ -9,9 +7,10 @@ def distance(document1, document2):
 	sim = sum(pow(document1-document2,2))
 	return sim
 
+
 #Returns a distance-base similarity score for document1 and document2
 def similarity(testgamma):
-    f=open("../../data/LDAResult/b_similarity.dat","w+")
+    f=open("../../data/LDAResult/b_similarity2.dat","w+")
     similarity_matrix = dict()
     row=1
     for i in testgamma:
@@ -19,7 +18,8 @@ def similarity(testgamma):
         temp = dict()
         for j in testgamma[:-1]:
             temp[col]=distance(i,j)
-            f.write(str(temp[col])+",")
+            #f.write(str(temp[col])+",") #Save in csv format
+            f.write(str(temp[col])+" ") #Save in txt format
             col=col+1
         temp[col]=distance(i,testgamma[-1])
         similarity_matrix[row]=temp
@@ -29,6 +29,7 @@ def similarity(testgamma):
     f.close()
     return similarity_matrix
 
+
 #Recommend a similar document based on similarity score
 #For different input data, use recommend command respectively
 #recommend2 already has a label, where recommend needs to use zip to get a label
@@ -36,17 +37,17 @@ def recommend(document_id,similarity_matrix):
     sim = similarity_matrix[document_id]
     temp = zip(sim, range(0, len(sim)))
     rank = sorted(temp, key = lambda x: x[0])
-    #return [rank[1][1],rank[2][]]
-    return rank[1][1]
+    return [rank[0][0],rank[1][0],rank[2][0],rank[3][0],rank[4][0]]
+
 
 def recommend2(document_id,similarity_matrix):
     sim = similarity_matrix[document_id]
     rank = sorted(sim.items(),key=operator.itemgetter(1))
-    #return [rank[1][0],rank[2][0]]
-    return rank[1][0]
+    return [rank[0][1],rank[1][1],rank[2][1],rank[3][1],rank[4][1]]
+
 
 def main():
-    #Combine all the gamma into one
+    #Combine all the gamma into one(need to run once if not)
     '''
     fi=open("../../data/LDAResult/allgamma.dat","w+")
     iteration = 50
@@ -60,12 +61,11 @@ def main():
     fi.close()
     '''
 
+
     #read allgamma from txt or csv file
     testgamma = numpy.loadtxt(file("../../data/LDAResult/allgamma.dat"))
     #testgamma = numpy.genfromtxt(file("../../data/LDAResult/allgamma.dat"),delimiter=',')
-
     gamma_size = len(testgamma)
-
     #Normalize data
     for k in range(0, gamma_size):
         temp_gamma = list(testgamma[k, :])
@@ -76,9 +76,11 @@ def main():
     #If similarity has been already generated, read it directly.
     #sim_matrix =  numpy.genfromtxt(file("../../data/LDAResult/b_similarity.dat"),delimiter=',')
 
+
+    #To test result
+    '''
     dp=dataParse.dataParse("../../data/ideas.txt")
     des=dp.concatedField("../../data/fieldList.txt")
-
     #Type the document ID to find its recommendations
     document_id = 96
 
@@ -86,13 +88,9 @@ def main():
     print r_id
     print des[document_id]
 
-    
-    '''
-    for i in r_id:
-        print "-----------------------------------------------------------------------------"
-        print des[4000+i]
-    '''
     print des[r_id]
+    '''
+
 
 if __name__ == '__main__':
     main()
