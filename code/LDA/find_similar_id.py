17e6953340c07153cc7a6a1ac42c79c9f1ff8acd
WildVegeJuice <jiahushen2@gmail.com>
2015-07-21 10:28:00 +0900

    Update find_similar_id.py

--- code/LDA/find_similar_id.py
+++ code/LDA/find_similar_id.py
@@ -1,18 +1,16 @@
+import dataParse
 import numpy
 
-def find_type(document_id,search_id):
-    f = open("../../data/other/type.txt")
+def find_type(document_id,search_id,dp):
+    type_of_innovation=dp.fieldParse("type_of_innovation")
     similar_id = []
-    l = f.readline().replace(',',' ')
-    type_of_innovation = l.split()
-    f.close()
 
     if type_of_innovation[document_id] == "None":
         return search_id
 
     j = 0
     for si in search_id:
-        if  type_of_innovation[si] == type_of_innovation[document_id]:
+        if type_of_innovation[si] == type_of_innovation[document_id]:
             similar_id.append(search_id[j])
         j = j+1
 
@@ -20,15 +18,12 @@ def find_type(document_id,search_id):
 
 
 def find_tag(document_id,search_id):
-    #Create a set for tags
-    tag_set = set()
-    f = open("../../data/other/tag.txt")
-    li = f.readline().replace(',',' ')
-    word = li.split()
-    for item in word:
-        tag_set.add(item)
+    f = open("../../data/Other/tag_set.txt")
+    tagset = f.readline().split()
+    tag_distribution = []
+    for item in range(0,len(tagset)):
+        tag_distribution.append(f.readline().split())
     f.close()
-
 
     #Read tags in id-th document
     line = open("../../data/document/tag"+str(document_id)+".txt").readline()
@@ -39,38 +34,20 @@ def find_tag(document_id,search_id):
 
 
     #Get the id of these tags in the whole tag set
-    tag_id = []
-    for tag in tags_in_search:
-        j = 0
-        for i in tag_set:
-            if i == tag:
-                tag_id.append(j)
-            j = j+1
-
-
-    #Create the document ids set for each tag
-    tag_belongings_set = []
-    for tag in tag_set:
-        temp = set()
-        for k in search_id:
-            line = open("../../data/document/tag"+str(k)+".txt").readline()
-            l = line.replace(',',' ')
-            tags = l.split(' ')
-            for tag_compare in tags:
-                if tag_compare == tag:
-                    temp.add(k)
-        tag_belongings_set.append(temp)
-
-
-    #Get docuemnt_id which share with the same tags
+    temp_id = []
+    for word in tags_in_search:
+        i = 0
+        for tag in tagset:
+            if word == tag:
+                temp_id.append(i)
+            i = i+1
     similar_id = set()
-    for id in tag_id:
-        for i in tag_belongings_set[id]:
-            similar_id.add(i)
-    if len(similar_id) == 1:
-        return search_id
-    else:
-        return similar_id
+    for i in temp_id:
+        for j in tag_distribution[i]:
+            if int(j) in search_id:
+                similar_id.add(int(j))
+
+    return similar_id
 
 
 def recommend(document_id,similarity_matrix,search_id):
@@ -102,16 +79,21 @@ def find_similar(document_id,search_id):
 
 
 def main():
+
+    dp=dataParse.dataParse("../../data/ideas.txt")
+
     number_of_documents = 5000
-    document_id = 120
-    search_id = range(0,number_of_documents)
-    search_id1 = find_type(document_id,search_id)
+    document_id = 4097
+    search_id = range(4000,4200)
+    search_id1 = find_type(document_id,search_id,dp)
     print search_id1
+
     search_id2 = list(find_tag(document_id,search_id1))
     print search_id2
-    similar_id = find_similar(document_id,search_id)
-    print similar_id
 
+
+    similar_id = find_similar(document_id,search_id2)
+    print similar_id+1
 
 if __name__ == '__main__':
     main()
